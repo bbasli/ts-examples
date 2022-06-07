d2071bc6fb92449462dbfb0ee3236323287b9479
bbasli <43937051+bbasli@users.noreply.github.com>
2022-06-08 00:47:07 +0300

    Update generics.ts

--- generics.ts
+++ generics.ts
@@ -60,3 +60,40 @@ const pokemons: Pokemon[] = [
 
 const ranks = ranker(pokemons, ({ str }) => str);
 console.log(ranks);
+
+function pluck<DataType, KeyType extends keyof DataType>(
+  items: DataType[],
+  key: KeyType
+): DataType[KeyType][] {
+  return items.map((item) => item[key]);
+}
+
+console.log(pluck(pokemons, "hp"));
+console.log(pluck(pokemons, "name"));
+console.log(pluck(pokemons, "str"));
+
+interface BaseEvent {
+  time: number;
+  user: string;
+}
+
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productID: string };
+  checkout: BaseEvent;
+}
+
+function sendEvent<Name extends keyof EventMap>(
+  name: Name,
+  data: EventMap[Name]
+): void {
+  console.log([name, data]);
+}
+
+sendEvent("addToCart", {
+  quantity: 3,
+  productID: "qwe123",
+  time: 1,
+  user: "me",
+});
+
+sendEvent("checkout", { user: "joe", time: 2 });
